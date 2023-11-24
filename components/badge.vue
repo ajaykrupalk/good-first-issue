931ee2b1de9b2925aeff95ea9b149ba2e343ffea
ajaykrupalk <ajaykrupalk@outlook.in>
2023-11-24 22:57:52 +0530

    Initial Commit

---
+++ components/badge.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <span
+            class="tracking-wide align-middle rounded-3xl bg-blue-50 py-0.5 px-2 font-bold text-blue-500 ring-1 ring-inset ring-blue-600/10"
+            style="font-size: 10px">
+            good first issue
+        </span>
+    </div>
+</template>
---
+++ components/sampleIssue.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="mt-3 overflow-hidden w-max rounded-md border-2 border-gray-200 p-2">
+        <div class="flex flex-row gap-1 items-baseline no-wrap">
+            <img src="../assets/images/issue.png" alt="issue icon" class="h-4 w-4">
+            <h3 class="font-medium text-gray-300 text-lg">Mismatch in validation</h3>
+            <badge></badge>
+        </div>
+        <p class="text-gray-300 w-full text-xs">#100 opened on Nov 21, 2023 by ajaykrupalk</p>
+    </div>
+</template>
